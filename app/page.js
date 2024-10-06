22fd18c38edee685d1f38b3d7583c93b3746ae11
Yousef Na <yousefnasr0541@gmail.com>
2024-10-06 20:42:13 +0300

    add: slideshow component

--- app/page.js
+++ app/page.js
@@ -1,12 +1,16 @@
 import Link from 'next/link';
 
+import { ImageSlideshow } from '@/components';
+
 import styles from './page.module.css';
 
 export default function Home() {
   return (
     <>
       <header className={styles.header}>
-        <div className={styles.slideshow}></div>
+        <div className={styles.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={styles.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
--- components/index.js
+++ components/index.js
@@ -1,1 +1,2 @@
 export { default as MainHeader } from './main-header/main-header';
+export { default as ImageSlideshow } from './images/image-slideshow';
