e7f11adfb62869378745adc90f072e5f69b4dc39
Yousef Na <yousefnasr0541@gmail.com>
2024-10-08 16:36:01 +0300

    add: single meal page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,8 +1,35 @@
 import React from 'react';
 
-function Meal({ params }) {
-  const { slug } = params;
-  return <div>{slug}</div>;
+import styles from './page.module.css';
+import { getMeal } from '@/lib/meals';
+import Image from 'next/image';
+
+function MealDetails({ params }) {
+  console.log(params.slug);
+  const meal = getMeal(params.slug);
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.alt} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>meal.creator</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={styles.main}>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        />
+      </main>
+    </>
+  );
 }
 
-export default Meal;
+export default MealDetails;
---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>404 - Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </main>
+  );
+}
+
+export default NotFound;
