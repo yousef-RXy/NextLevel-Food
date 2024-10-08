c944fee0c990db741ff5d887753251bd9ebca450
Yousef Na <yousefnasr0541@gmail.com>
2024-10-08 15:41:22 +0300

    add: error handling

---
+++ app/meals/error.js
@@ -0,0 +1,12 @@
+'use client';
+
+function Error({ error }) {
+  return (
+    <main className="error">
+      <h1>An error occurred</h1>
+      <p>failed to fetch</p>
+    </main>
+  );
+}
+
+export default Error;
